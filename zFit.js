68c96ee62eb9d5ebac324f6629914751cedda09e
Zachary Skalko <zskalko@gmail.com>
2014-06-16 01:52:47 -0500

    tested with nowrap

--- zFit.js
+++ zFit.js
@@ -7,22 +7,28 @@ this.each(function(i){
 	var theElementHeight = $theElement.height();
 	var theElementWidth = $theElement.width();
 
-	$theElement.wrapInner('<div style="display:table;height:'+theElementHeight+'px;width:'+theElementWidth+'px"><div class="zFit" style="width:100%;vertical-align:middle;"></div></div>');
-
+	$theElement.wrapInner('<div style="display:table;height:'+theElementHeight+'px;width:'+theElementWidth+'px"><div class="zFit" style="vertical-align:middle;display:inline-block;"></div></div>');
 	var $zFit = $theElement.find(".zFit");
 	var zFitHeight = $zFit.height();
 	var zFitWidth = $zFit.width();
 
 	function makeFit(estimate){
+	
+		if (estimate < .6){
+
+		$zFit.css("white-space","normal");
+		$zFit.addClass("zTiny");
+
+		}
 
 		$zFit.css("font-size",estimate+"em");
 
 		
 		if (theElementHeight < $zFit.height() || theElementWidth < $zFit.width()){
-			makeFit((estimate *.9))
+			makeFit((estimate *.8).toFixed(2))
 
 		}else{
-			if(allSameSize){
+			if(allSameSize && estimate >= .6){
 				sizes.push(estimate);
 			}
 			$zFit.css("display","table-cell");
@@ -30,23 +36,27 @@ this.each(function(i){
 
 	}
 
-	var estimated = [];
-//	estimated.push((theElementHeight * theElementWidth) / (zFitHeight * zFitWidth));
-	estimated.push((theElementHeight) / (zFitHeight));
-	estimated.push((theElementWidth) / (zFitWidth));
-
-
-	if ($.grep(estimated, function(n,i){return n > 1}).length = 0){
-		alert(1/(Math.min.apply(Math, estimated)));
-		makeFit(1/(Math.min.apply(Math, estimated)));
-	}else{
-		alert(1/(Math.max.apply(Math, estimated)));
-		makeFit(1/(Math.max.apply(Math, estimated)));
+	var estimated;
+	
+	if ((theElementWidth / zFitWidth) == 1){
+	estimated = (theElementHeight / zFitHeight);
+//	alert("ratio:" + estimated + "\nElement Height:" + theElementHeight + "\nzHeight:" + zFitHeight);
 	}
+	else
+	{
+	
+	estimated = (theElementWidth / zFitWidth);
+//	alert("ratio:" + estimated + "\nElement Width:" + theElementWidth + "\nzWidth:" + zFitWidth);
+	}
+
+
+
+makeFit(estimated.toFixed(2));
+
 });
 
 if(allSameSize){
-this.find(".zFit").css("font-size", Math.min.apply(Math, sizes)+"em");
+this.find(".zFit").not(".zTiny").css("font-size", Math.min.apply(Math, sizes)+"em");
 }
 
 }
