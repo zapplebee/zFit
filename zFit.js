e93a3825b6fabdcd8dd9f0a616f8dd8ac4199028
Zachary Skalko <zskalko@gmail.com>
2014-06-15 22:29:57 -0500

    cleaned up the whitespace

--- zFit.js
+++ zFit.js
@@ -1,40 +1,39 @@
 $.fn.zFit = function(){
 
+	var $theElement = this;
+	var theElementHeight = $theElement.height();
+	var theElementWidth = $theElement.width();
+
+	$theElement.wrapInner('<div style="display:table;height:'+theElementHeight+'px;width:'+theElementWidth+'px"><div class="zFit" style="width:100%;vertical-align:middle;"></div></div>');
+
+	var $zFit = $theElement.find(".zFit");
+	var zFitHeight = $zFit.height();
+	var zFitWidth = $zFit.width();
+
+	function makeFit(estimate){
+
+		$zFit.css("font-size",estimate+"em");
+
+		if (theElementHeight < $zFit.height() || theElementWidth < $zFit.width()){
+			makeFit((estimate -.1))
+
+		}else{
+			$zFit.css("display","table-cell");
+		}
+
+	}
+
+	var estimated = [];
+	estimated.push((theElementHeight * theElementWidth) / (zFitHeight * zFitWidth));
+	estimated.push((theElementHeight) / (zFitHeight));
+	estimated.push((theElementWidth) / (zFitWidth));
 
 
-var $theElement = this;
-var theElementHeight = $theElement.height();
-var theElementWidth = $theElement.width();
+	if ($.grep(estimated, function(n,i){return n > 1}).length = 0){
+		makeFit(Math.min.apply(Math, estimated));
+	}else{
+		makeFit(Math.max.apply(Math, estimated));
+	}
 
-
-$theElement.wrapInner('<div style="display:table;height:'+theElementHeight+'px;width:'+theElementWidth+'px"><div class="zFit" style="width:100%;vertical-align:middle;"></div></div>');
-
-var $zFit = $theElement.find(".zFit");
-var zFitHeight = $zFit.height();
-var zFitWidth = $zFit.width();
-function makeFit(estimate){
-
-$zFit.css("font-size",estimate+"em");
-
-if (theElementHeight < $zFit.height() || theElementWidth < $zFit.width()){
-makeFit((estimate -.1))
-
-}else{
-$zFit.css("display","table-cell");
-}
-
-}
-
-var estimated = [];
-estimated.push((theElementHeight * theElementWidth) / (zFitHeight * zFitWidth));
-estimated.push((theElementHeight) / (zFitHeight));
-estimated.push((theElementWidth) / (zFitWidth));
-
-
-if ($.grep(estimated, function(n,i){return n > 1}).length = 0){
-makeFit(Math.min.apply(Math, estimated));
-}else{
-makeFit(Math.max.apply(Math, estimated));
-}
 
 }
