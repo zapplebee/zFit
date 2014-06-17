b51f78a9edabef999b7a91df05d5de49b33049c8
Zachary Skalko <zskalko@gmail.com>
2014-06-16 20:10:30 -0500

    fixed the height fit issue
    There are a lot more calculations now to find the right size, but the
    height issue is fixed.

--- zFit.js
+++ zFit.js
@@ -1,6 +1,4 @@
-$.fn.zFit = function(tolerance, allSameSize){
-
-tolerance = tolerance || 30;
+$.fn.zFit = function(allSameSize){
 allSameSize = allSameSize || false;
 var sizes = [];
 this.each(function(i){
@@ -10,44 +8,29 @@ this.each(function(i){
 
 	$theElement.wrapInner('<div style="display:table;height:'+theElementHeight+'px;width:'+theElementWidth+'px"><div class="zFit" style="vertical-align:middle;display:inline-block;"></div></div>');
 	var $zFit = $theElement.find(".zFit");
-	var zFitHeight = $zFit.height();
-	var zFitWidth = $zFit.width();
 
 	function makeFit(estimate){
 	
 
 		$zFit.css("font-size",(estimate)+"em");
 
-//		alert("theElementHeight:"+theElementHeight+"\n$zFit.height():"+$zFit.height()+"\ntheElementWidth:"+theElementWidth+"\n$zFit.width():"+$zFit.width());
-		if ((theElementHeight) >= $zFit.height() && (theElementWidth) >= $zFit.width()){
-//			alert("true");
-//			alert(zestimate);
-			makeFit(estimate * 1.1);
+		if (((theElementHeight) < $zFit.height()) || ((theElementWidth) < $zFit.width())){
 
+			var oldEM = estimate * (1/1.05);
+			sizes.push(oldEM);
+			$zFit.css("font-size",oldEM+"em");
+			$zFit.css("display","table-cell");
+			
+		}else{
+		
+		makeFit(estimate * 1.05);
+		
 		}
-		else{
-		$zFit.css("font-size",(estimate * (1/1.1))+"em");
-		$zFit.css("display","table-cell");
-		}
-	}
-
-	var estimated;
-	
-	if ((theElementWidth / zFitWidth) <= 1){
-	estimated = (theElementHeight / zFitHeight);
-//	alert("ratio:" + estimated + "\nElement Height:" + theElementHeight + "\nzHeight:" + zFitHeight);
-	}
-	else
-	{
-	
-	estimated = (theElementWidth / zFitWidth);
-//	alert("ratio:" + estimated + "\nElement Width:" + theElementWidth + "\nzWidth:" + zFitWidth);
 	}
 
 
+	makeFit(.01);
 
-makeFit(estimated.toFixed(2));
-//alert($zFit.attr("style"));
 });
 
 if(allSameSize){
